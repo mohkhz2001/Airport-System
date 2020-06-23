9e07de8fe912861425cbc4cc02bb7b141803c76f
Mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2020-06-23 09:54:27 +0430

    query on the flight Table with class flight

---
+++ src/Model/FlightRepository.java
@@ -0,0 +1,61 @@
+package Model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlightRepository {
+
+    Connection connection = new Connection();
+
+    public List<Flight> flightList() {
+        Statement statement;
+        ResultSet resultSet;
+
+        try {
+            statement = connection.connection().createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM " + Tables.TABLE_FLIGHT);
+
+            ArrayList<Flight> flightArrayList = new ArrayList<>();
+
+            while (resultSet.next()) {
+
+                Flight flight = new Flight();
+
+                flight.setFlightNumber(resultSet.getString(Tables.TABLE_FLIGHT_FLIGHTNUMBER));
+                flight.setAirplaneRegister(resultSet.getString(Tables.TABLE_FLIGHT_AIRPLANEREGISTER));
+                flight.setTicketID(resultSet.getString(Tables.TABLE_FLIGHT_TICKETID));
+                flight.setDep(resultSet.getString(Tables.TABLE_FLIGHT_DEP));
+                flight.setDes(resultSet.getString(Tables.TABLE_FLIGHT_DES));
+                flight.setDate(resultSet.getString(Tables.TABLE_FLIGHT_DATE));
+                flight.setHours(resultSet.getString(Tables.TABLE_FLIGHT_HOUR));
+                flight.setFlightTime(resultSet.getString(Tables.TABLE_FLIGHT_TIMEFLIGHT));
+                flight.setCapacity(resultSet.getInt(Tables.TABLE_FLIGHT_CAPACITY));
+                flight.setPrice(resultSet.getInt(Tables.TABLE_FLIGHT_PRICE));
+
+                if (resultSet.getString(Tables.TABLE_FLIGHT_STATUS).equals("open")) {
+                    flight.setStatus(Flight.status.open);
+                } else if (resultSet.getString(Tables.TABLE_FLIGHT_STATUS).equals("done")) {
+                    flight.setStatus(Flight.status.done);
+                } else {
+                    flight.setStatus(Flight.status.takeoff);
+                }
+
+                flightArrayList.add(flight);
+            }
+
+            return flightArrayList;
+
+        } catch (SQLException throwables) {
+
+            System.out.println("theres problem in flight repo \n" + throwables);
+            return null;
+
+        }
+
+
+    }
+
+}
