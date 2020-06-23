5998160c62168d3ddb779991a35bcae7ec5d6a9a
Mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2020-06-23 09:46:32 +0430

    the tables name and another info about the DB query

---
+++ src/Model/Tables.java
@@ -0,0 +1,48 @@
+package Model;
+
+public class Tables {
+    public static final String TABLE_PASSENGER = "passenger";
+    public static final String TABLE_PASSENGER_FIRSTNAME = "firstname";
+    public static final String TABLE_PASSENGER_LASTNAME = "lastname";
+    public static final String TABLE_PASSENGER_USERNAME = "username";
+    public static final String TABLE_PASSENGER_PASSWORD = "password";
+    public static final String TABLE_PASSENGER_PASSID = "PassID";
+    public static final String TABLE_PASSENGER_EMAIL = "email";
+    public static final String TABLE_PASSENGER_MONEY = "money";
+//    public static final String TABLE_PASSENGER_ ="Passenger";
+
+
+    public static final String TABLE_USER = "user";
+    public static final String TABLE_USER_FIRSTNAME = "firstname";
+    public static final String TABLE_USER_LASTNAME = "lastname";
+    public static final String TABLE_USER_USERNAME = "username";
+    public static final String TABLE_USER_PASSWORD = "password";
+    public static final String TABLE_USER_EMAIL = "email";
+    public static final String TABLE_USER_ID = "ID";
+    public static final String TABLE_USER_JOB = "job";
+    public static final String TABLE_USER_PHONENUMBER = "phoneNumber";
+    public static final String TABLE_USER_ADDRESS = "address";
+    public static final String TABLE_USER_SALARY = "salary";
+
+
+    public static final String TABLE_FLIGHT = "Flight";
+    public static final String TABLE_FLIGHT_FLIGHTNUMBER = "flight Number";
+    public static final String TABLE_FLIGHT_AIRPLANEREGISTER = "airplane register";
+    public static final String TABLE_FLIGHT_TICKETID = "ticket ID";
+    public static final String TABLE_FLIGHT_DEP = "dep";
+    public static final String TABLE_FLIGHT_DES = "des";
+    public static final String TABLE_FLIGHT_DATE = "date";
+    public static final String TABLE_FLIGHT_HOUR = "hour";
+    public static final String TABLE_FLIGHT_CAPACITY = "capacity";
+    public static final String TABLE_FLIGHT_TIMEFLIGHT = "time flight";
+    public static final String TABLE_FLIGHT_STATUS = "status";
+    public static final String TABLE_FLIGHT_PRICE= "price";
+
+
+    public static final String TABLE_AIRPLANE = "airplane";
+    public static final String TABLE_AIRPLANE_REGISTER = "register";
+    public static final String TABLE_AIRPLANE_TYPE = "type";
+    public static final String TABLE_AIRPLANE_SEAT = "seat";
+
+
+}
