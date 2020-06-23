768c58ae6050d5a16e517bcd40c4b16b9a3152ff
Mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2020-06-23 09:48:24 +0430

    the airplane class that have the airplane  ability

---
+++ src/Model/Airplane.java
@@ -0,0 +1,31 @@
+package Model;
+
+public class Airplane {
+    private String register;
+    private String type;
+    private int seats;
+
+    public String getRegister() {
+        return register;
+    }
+
+    public void setRegister(String register) {
+        this.register = register;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+}
