b013c5845c1d3cf5c744d6dbb2ade19bb818d6c4
Mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2020-06-24 02:01:22 +0430

    change the profile graphic part and added some action to controller class 1.if peres any key at the email field => email field will be edit 2.if press ant key at the password field the re password field will be active 3.and borders will be change

--- src/Controller/ProfileConroller.java
+++ src/Controller/ProfileConroller.java
@@ -1,20 +1,90 @@
 package Controller;
 
+import Model.PassengerRepository;
+import Model.passenger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ProfileConroller implements Initializable {
+    private String ID;
+
 
     @FXML
     ImageView personImage;
+    @FXML
+    TextField fullNameField;
+    @FXML
+    TextField idField;
+    @FXML
+    PasswordField passwordField;
+    @FXML
+    PasswordField rePasswordField;
+    @FXML
+    TextField jobField;
+    @FXML
+    TextField emailField;
+    @FXML
+    Button conffirmBTN;
+
+    public void passwordField() {
+        passwordField.setStyle("-fx-border-width: 0 0 3 0 ; -fx-border-color: green ; -fx-background-color: transparent");
+        rePasswordField.setStyle("-fx-border-width: 0 0 3 0 ; -fx-border-color: green ; -fx-background-color: transparent");
+        conffirmBTN.setDisable(false);
+    }
+
+    public void emailField() {
+        emailField.setStyle("-fx-border-width: 0 0 3 0 ; -fx-border-color: green ; -fx-background-color: transparent");
+        conffirmBTN.setDisable(false);
+
+    }
+
+    public void conffirmBTN() {
+
+    }
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         personImage.setImage(new Image("file:Icons/person.png"));
+//        show();
+
+    }
+
+    public void show(String ID) {
+        this.ID = ID;
+//        System.out.println(getID());
+
+        PassengerRepository passengerRepository = new PassengerRepository();
+        List<passenger> passengerList = passengerRepository.passengerList();
+
+        for (int i = 0; i < passengerList.size(); i++) {
+
+            if (passengerList.get(i).getID().equals(getID())) {
+
+                fullNameField.setText(passengerList.get(i).getFirstName() + " " + passengerList.get(i).getLastName());
+                idField.setText(passengerList.get(i).getID());
+                passwordField.setText(passengerList.get(i).getPassword());
+                emailField.setText(passengerList.get(i).getEmail());
+                jobField.setText(String.valueOf(passengerList.get(i).getJob()));
+
+            }
+        }
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
     }
 }
