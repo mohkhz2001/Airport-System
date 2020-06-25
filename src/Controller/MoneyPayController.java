5176544392ef731376ed0fa389a44808e3976faf
Mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2020-06-25 10:16:34 +0430

    1. payBTN connected to DB and work correctly 2. paybtn have enter and exit style 3. progressbar filter added....

--- src/Controller/MoneyPayController.java
+++ src/Controller/MoneyPayController.java
@@ -6,6 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 
 import java.net.URL;
@@ -45,12 +46,16 @@ public class MoneyPayController implements Initializable {
                 amountField.setText(newValue.replaceAll("[^\\d]", ""));
             }
         });
-        progressBar.setProgress(.2);
+        if (progressBar.getProgress() < .2)
+            progressBar.setProgress(.2);
     }
 
     public void dateField() {
+
         dateField.setStyle("-fx-background-color: transparent ; -fx-border-width: 0 0 3 0 ; -fx-border-color: green");
-        progressBar.setProgress(.6);
+        if (progressBar.getProgress() < .6)
+            progressBar.setProgress(.6);
+
     }
 
     public void cardNumberField() {
@@ -61,45 +66,97 @@ public class MoneyPayController implements Initializable {
             cardNumberField.appendText(" - ");/// important
             counter = 1;
         }
-        progressBar.setProgress(.4);
+        if (progressBar.getProgress() < .4)
+            progressBar.setProgress(.4);
     }
 
     public void cvv2Field() {
+        cvv2Field.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                passField.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });
         cvv2Field.setStyle("-fx-background-color: transparent ; -fx-border-width: 0 0 3 0 ; -fx-border-color: green");
-        progressBar.setProgress(.8);
+        if (progressBar.getProgress() < .8)
+            progressBar.setProgress(.8);
     }
 
     public void passField() {
+        passField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                passField.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });
         passField.setStyle("-fx-background-color: transparent ; -fx-border-width: 0 0 3 0 ; -fx-border-color: green");
-        progressBar.setProgress(1);
+        if (progressBar.getProgress() < 1)
+            progressBar.setProgress(1);
     }
 
     public void emailField() {
         emailField.setStyle("-fx-background-color: transparent ; -fx-border-width: 0 0 3 0 ; -fx-border-color: green");
     }
 
-    public void payBTN(){
-//        if ()
+    public void payBTN() {
 
-        PassengerRepository passengerRepository = new PassengerRepository();
-        List<passenger> passengerList = passengerRepository.passengerList();
-        for (int i = 0; i < passengerList.size(); i++) {
-            if (passengerList.get(i).getID().equals(getID())){
+        if (rulesCheckBox.isSelected() && !amountField.getText().isEmpty() && !dateField.getText().isEmpty() && !cardNumberField.getText().isEmpty() &&
+                !cvv2Field.getText().isEmpty() && !passField.getText().isEmpty()) {
 
-                int a = Integer.parseInt(passengerList.get(i).getMoney()) + Integer.parseInt(amountField.getText());
-                passengerList.get(i).setMoney(Integer.toString(a));
-                break;
+            PassengerRepository passengerRepository = new PassengerRepository();
+            List<passenger> passengerList = passengerRepository.passengerList();
+
+            for (int i = 0; i < passengerList.size(); i++) {
+
+                if (passengerList.get(i).getID().equals(getID())) {
+
+                    int a = Integer.parseInt(passengerList.get(i).getMoney()) + Integer.parseInt(amountField.getText());
+
+                    passengerRepository.increaseMoney(Integer.toString(a), passengerList.get(i).getID());
+                    break;
+
+                }
 
             }
 
+        } else {
+
+            if (!rulesCheckBox.isSelected()) {
+                rulesCheckBox.setTextFill(Color.RED);
+            }
+            if (amountField.getText().isEmpty()) {
+                amountField.setStyle("-fx-background-color: transparent ; -fx-border-color: red ; -fx-border-width:  0 0 3 0");
+            }
+            if (dateField.getText().isEmpty()) {
+                dateField.setStyle("-fx-background-color: transparent ; -fx-border-color: red ; -fx-border-width:  0 0 3 0");
+            }
+            if (passField.getText().isEmpty()) {
+                passField.setStyle("-fx-background-color: transparent ; -fx-border-color: red ; -fx-border-width:  0 0 3 0");
+            }
+            if (cvv2Field.getText().isEmpty()) {
+                cvv2Field.setStyle("-fx-background-color: transparent ; -fx-border-color: red ; -fx-border-width:  0 0 3 0");
+            }
+            if (cardNumberField.getText().isEmpty()) {
+                cardNumberField.setStyle("-fx-background-color: transparent ; -fx-border-color: red ; -fx-border-width:  0 0 3 0");
+            }
+
         }
+
+
+    }
+
+    public void payBTNEnter() {
+        payBTN.setGraphic(new ImageView("file:Icons/pay.png"));
+        payBTN.setText(null);
+    }
+
+    public void payBTNExit() {
+        payBTN.setText("PAY");
+        payBTN.setGraphic(null);
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         progressBar.setDisable(true);
         progressBar.setProgress(0);
-
 
 
     }
@@ -112,7 +169,5 @@ public class MoneyPayController implements Initializable {
         this.ID = ID;
     }
 
-//    public boolean check(){
-//        String b = amountField.getText();
-//    }
+
 }
