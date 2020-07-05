0106db8ede81d2ce2c83c7d9bea78efe3c73dce0
Mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2020-07-06 01:29:29 +0430

    1.int this page you can see the flight info and edit some of them 2.after second click on the table and click on the edit flight this page will be show 3.if you click on the update BTN the info will be update....

---
+++ src/Controller/EditFlightController.java
@@ -0,0 +1,199 @@
+package Controller;
+
+import Model.Flight;
+import Model.FlightRepository;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import org.w3c.dom.ls.LSOutput;
+
+import java.awt.event.ActionListener;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EditFlightController implements Initializable {
+
+    Flight flight;
+
+    @FXML
+    ImageView image;
+    @FXML
+    TextField registerField;
+    @FXML
+    TextField FNField;
+    @FXML
+    TextField depField;
+    @FXML
+    TextField desField;
+    @FXML
+    TextField dateField;
+    @FXML
+    TextField hoursField;
+    @FXML
+    TextField priceField;
+    @FXML
+    TextField flightTimeField;
+    @FXML
+    TextField totalCapaField;
+    @FXML
+    TextField availabelCapaField;
+    @FXML
+    ChoiceBox<Flight.status> statusBox;
+    @FXML
+    Label registerLBL;
+    @FXML
+    Label FNLBL;
+    @FXML
+    Label depLBL;
+    @FXML
+    Label desLBL;
+    @FXML
+    Label dateLBL;
+    @FXML
+    Label hoursLBL;
+    @FXML
+    Label priceLBL;
+    @FXML
+    Label flightTimeLBL;
+    @FXML
+    Label statusLBL;
+    @FXML
+    Button updateBTN;
+
+
+    public void depField() {
+        if (!flight.getDep().equals(depField.getText())) {
+            depLBL.setTextFill(Color.GREEN);
+            depLBL.setText("departure" + " ** changed **");
+        } else {
+            depLBL.setTextFill(Color.BLACK);
+            depLBL.setText("departure");
+        }
+    }
+
+    public void updateBTN() {
+
+        FlightRepository flightRepository = new FlightRepository();
+
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "change " + flight.getFlightNumber() + " ?", ButtonType.YES, ButtonType.NO);
+        alert.showAndWait();
+
+        if (alert.getResult() == ButtonType.YES) {
+
+            boolean done = flightRepository.infoUpdate(FNField.getText(), depField.getText(), desField.getText(), dateField.getText(), hoursField.getText(), flightTimeField.getText(), statusBox.getValue());
+
+            if (done) {
+
+                Alert alert1 = new Alert(Alert.AlertType.INFORMATION, "change " + flight.getFlightNumber(), ButtonType.CLOSE);
+                alert1.showAndWait();
+                ((Stage) updateBTN.getScene().getWindow()).close();
+            } else {
+                Alert alert1 = new Alert(Alert.AlertType.ERROR, "cant update " + flight.getFlightNumber(), ButtonType.CLOSE);
+                alert1.showAndWait();
+                ((Stage) updateBTN.getScene().getWindow()).close();
+            }
+        }
+
+
+    }
+
+    public void desField() {
+        if (!flight.getDes().equals(desField.getText())) {
+            desLBL.setTextFill(Color.GREEN);
+            desLBL.setText("destination" + " ** changed **");
+        } else {
+            desLBL.setTextFill(Color.BLACK);
+            desLBL.setText("destination");
+        }
+    }
+
+    public void dateField() {
+        if (!flight.getDate().equals(dateField.getText())) {
+            dateLBL.setTextFill(Color.GREEN);
+            dateLBL.setText("date" + " ** changed **");
+        } else {
+            dateLBL.setTextFill(Color.BLACK);
+            dateLBL.setText("date");
+        }
+    }
+
+    public void hoursField() {
+        if (!flight.getHours().equals(hoursField.getText())) {
+            hoursLBL.setTextFill(Color.GREEN);
+            hoursLBL.setText("hour" + " ** changed **");
+        } else {
+            hoursLBL.setTextFill(Color.BLACK);
+            hoursLBL.setText("hour");
+        }
+    }
+
+    public void priceLBL() {
+        if (flight.getPrice() != Integer.parseInt(priceField.getText())) {
+            priceLBL.setTextFill(Color.GREEN);
+            priceLBL.setText("price" + " ** changed **");
+        } else {
+            priceLBL.setTextFill(Color.BLACK);
+            priceLBL.setText("price");
+        }
+
+    }
+
+    public void flightTimeField() {
+        if (!flight.getFlightTime().equals(flightTimeField.getText())) {
+            flightTimeLBL.setTextFill(Color.GREEN);
+            flightTimeLBL.setText("flight time" + " ** changed **");
+        } else {
+            flightTimeLBL.setTextFill(Color.BLACK);
+            flightTimeLBL.setText("flight time");
+        }
+    }
+
+    public void statusBox() {
+        if (!flight.getStatus().equals(statusBox.getItems())) {
+            statusLBL.setTextFill(Color.GREEN);
+            statusLBL.setText("status" + " ** changed **");
+        } else {
+            statusLBL.setTextFill(Color.BLACK);
+            statusLBL.setText("status");
+        }
+    }
+
+
+    public void closeBTN() {
+        ((Stage) registerField.getScene().getWindow()).close();
+    }
+
+    @Override
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+        image.setImage(new Image("file:Icons/airplane-edit.png"));
+        statusBox.getItems().addAll(Flight.status.open, Flight.status.done, Flight.status.takeoff);
+
+//        desField.getOnInputMethodTextChanged().handle(new InputMethodEvent());
+    }
+
+    public void showFlight(Flight info) {
+        flight = info;
+        registerField.setText(info.getAirplaneRegister());
+        FNField.setText(info.getFlightNumber());
+        depField.setText(info.getDep());
+        desField.setText(info.getDes());
+        dateField.setText(info.getDate());
+        hoursField.setText(info.getHours());
+        priceField.setText(Integer.toString(info.getPrice()));
+//        registerField.setText(info.getAirplaneRegister());
+        flightTimeField.setText(info.getFlightTime());
+        statusBox.setValue(info.getStatus());
+        totalCapaField.setText(Integer.toString(info.getTotalCapacity()));
+        availabelCapaField.setText(Integer.toString(info.getCapacity()));
+
+    }
+}
