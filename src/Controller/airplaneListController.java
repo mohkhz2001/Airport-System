d9a09d7004fbbe28934313618c4b2e6bd63c61bf
Mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2020-07-06 02:05:41 +0430

    1.two tables added 1-1.show the airplane 1-2. after click on the airplane the second table will be active and show the flight with that airplane 2.the second click added for the second table that show "passenger list , edit , remove" 3.remove part will be remove the flight and delete all the ticket that soled

--- src/Controller/airplaneListController.java
+++ src/Controller/airplaneListController.java
@@ -1,15 +1,212 @@
 package Controller;
 
+import Model.*;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import javax.swing.*;
+import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class airplaneListController implements Initializable {
 
+    AirplaneRepository airplaneRepository = new AirplaneRepository();
+    FlightRepository flightRepository = new FlightRepository();
+    TicketRepository ticketRepository = new TicketRepository();
+
+    ContextMenu contextMenu;
+    MenuItem passList;
+    MenuItem edit;
+    MenuItem remove;
+    Flight flight;
+
+
+    @FXML
+    TableView<Airplane> airplaneListTable;
+    @FXML
+    TableColumn<String, Airplane> registerColumn;
+    @FXML
+    TableColumn<String, Airplane> seatColumn;
+    @FXML
+    TableColumn<String, Airplane> typeColumn;
+
+    @FXML
+    TableColumn<String, Flight> flightNumberColumn;
+    @FXML
+    TableColumn<String, Flight> dateColumn;
+    @FXML
+    TableColumn<String, Flight> depColumn;
+    @FXML
+    TableColumn<String, Flight> desColumn;
+    @FXML
+    TableColumn<String, Flight> statusColumn;
+    @FXML
+    TableColumn<String, Flight> hoursColumn;
+    @FXML
+    TableView<Flight> flightListTable;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        TableShow();
+        ActionOnTable();
+        flightListAction();
+
+        contextMenu = new ContextMenu();
+        passList = new MenuItem("List of passenger");
+        edit = new MenuItem("Edit");
+        remove = new MenuItem("Remove");
+        contextMenu.getItems().addAll(passList, edit, remove);
+
+        flightListTable.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent t) {
+                if (t.getButton() == MouseButton.SECONDARY) {
+                    contextMenu.show(flightListTable, t.getScreenX(), t.getScreenY());
+                }
+            }
+        });
+
+        contextMenuAction();
 
     }
+
+    public void TableShow() {
+
+        registerColumn.setCellValueFactory(new PropertyValueFactory<>("register"));
+        seatColumn.setCellValueFactory(new PropertyValueFactory<>("seats"));
+        typeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
+
+        List<Airplane> airplaneList = airplaneRepository.airplaneList();
+
+        for (int i = 0; i < airplaneList.size(); i++) {
+            airplaneListTable.getItems().addAll(airplaneList.get(i));
+        }
+    }
+
+    private void ActionOnTable() {
+
+
+        airplaneListTable.setRowFactory(tv -> {
+            TableRow<Airplane> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (!row.isEmpty() && event.getButton() == MouseButton.PRIMARY
+                        && event.getClickCount() == 1) {
+
+                    Airplane clickedRow = row.getItem();
+                    flightsList(clickedRow);
+                    System.out.println("Done");
+                }
+
+            });
+            return row;
+        });
+    }
+
+    private void flightsList(Airplane info) {
+
+        flightListTable.getItems().clear();
+
+        flightNumberColumn.setCellValueFactory(new PropertyValueFactory<>("flightNumber"));
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
+        depColumn.setCellValueFactory(new PropertyValueFactory<>("dep"));
+        desColumn.setCellValueFactory(new PropertyValueFactory<>("des"));
+        statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
+        hoursColumn.setCellValueFactory(new PropertyValueFactory<>("hours"));
+
+        List<Flight> flights = flightRepository.flightList();
+
+        for (int i = 0; i < flights.size(); i++) {
+            if (flights.get(i).getAirplaneRegister().equals(info.getRegister())) {
+                flightListTable.getItems().addAll(flights.get(i));
+            }
+        }
+
+    }
+
+    private void flightListAction() {
+
+        flightListTable.setRowFactory(tv -> {
+            TableRow<Flight> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (!row.isEmpty() && event.getButton() == MouseButton.SECONDARY
+                        && event.getClickCount() == 1) {
+
+                    Flight clickedRow = row.getItem();
+                    flight = clickedRow;
+
+                    System.out.println("Done");
+                }
+
+            });
+            return row;
+        });
+    }
+
+    private void contextMenuAction() {
+
+        passList.setOnAction(event -> {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/PassengerList.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            PassengerListController passengerListController = loader.getController();
+            passengerListController.TableShow(flight);
+            Stage stage = new Stage();
+            stage.setScene(new Scene(loader.getRoot()));
+            stage.show();
+
+        });
+
+        edit.setOnAction(event -> {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/EditFlight.fxml"));
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            EditFlightController editFlightController = loader.getController();
+            editFlightController.showFlight(flight);
+
+            Stage stage = new Stage();
+            stage.setScene(new Scene(loader.getRoot()));
+//            stage.setResizable(false);
+            stage.show();
+        });
+
+        remove.setOnAction(event -> {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Delete " + flight.getFlightNumber() + " and delete all tickets soled " + " ?", ButtonType.YES, ButtonType.NO);
+            alert.showAndWait();
+
+            if (alert.getResult() == ButtonType.YES) {
+                boolean done = flightRepository.removeFlight(flight.getFlightNumber());
+
+                if (done) {
+                    Alert alert1 = new Alert(Alert.AlertType.INFORMATION, "removed " + flight.getFlightNumber(), ButtonType.CLOSE);
+                    alert1.showAndWait();
+                    ticketRepository.removeTicket(flight.getFlightNumber());
+                }
+            } else {
+                Alert alert1 = new Alert(Alert.AlertType.ERROR, "cant remove " + flight.getFlightNumber(), ButtonType.CLOSE);
+                alert1.showAndWait();
+            }
+
+        });
+    }
+
 }
