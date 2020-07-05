22a395969c2c54e6f970bbe635e2a378d51ef793
Mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2020-07-06 01:57:04 +0430

    1.new flight BTN added 2.actions are added 3.airplane actions added(some of them updated)

--- src/Controller/FlightManagerController.java
+++ src/Controller/FlightManagerController.java
@@ -18,6 +18,8 @@ public class FlightManagerController implements Initializable {
     SplitPane split;
     @FXML
     Button airplaneBTN;
+    @FXML
+    Button NewFlightBTN;
 
 
     public void flightsBTN() {
@@ -38,22 +40,47 @@ public class FlightManagerController implements Initializable {
         flightsBTN.setStyle("-fx-background-color: transparent ");
     }
 
-    public void airplaneBTN(){
+    public void airplaneBTN() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/airplaneList.fxml"));
 
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        split.getItems().set(1, loader.getRoot());
     }
 
-    public void airplaneBTNEnter(){
+    public void airplaneBTNEnter() {
         airplaneBTN.setStyle("-fx-background-color: #3679c6");
     }
 
-    public void airplaneBTNExit(){
+    public void airplaneBTNExit() {
         airplaneBTN.setStyle("-fx-background-color: transparent");
+    }
+
+    public void NewFlightBTN(){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/NewFlight.fxml"));
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        split.getItems().set(1 , loader.getRoot());
+    }
+
+    public void NewFlightBTNEnter(){
+        NewFlightBTN.setStyle("-fx-background-color: #3679c6");
+    }
+
+    public void NewFlightBTNExit(){
+        NewFlightBTN.setStyle("-fx-background-color: transparent ");
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 //        split.setDividerPositions(0.2);
         split.lookupAll(".split-pane-divider").stream()
-                .forEach(div ->  div.setMouseTransparent(true) );
+                .forEach(div -> div.setMouseTransparent(true));
     }
 }
