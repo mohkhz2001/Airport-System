c17123a94d2652269de3f9b674b94e5153e491de
Mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2020-07-20 01:00:06 +0430

    1. manager can see the kist of the employee + new +info about the employee.

---
+++ src/Controller/EmployeeBTNController.java
@@ -0,0 +1,35 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EmployeeBTNController implements Initializable {
+
+    @FXML
+    Button ListBTN;
+    @FXML
+    Button newBTN;
+
+
+    public void ListBTN() {
+
+    }
+
+    public void newBTN() {
+
+    }
+
+    public void infoBTN(){
+
+
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
