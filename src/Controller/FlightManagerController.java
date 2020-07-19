b1cb6ee6452b3fdaf12c73033e5ef90858257dc8
Mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2020-07-20 00:08:19 +0430

    1.airplane BTN controller added 2.management functions added that have the new btn for the manager login (new airplane) + action btn added 3.get and set job added 4. get job added

--- src/Controller/FlightManagerController.java
+++ src/Controller/FlightManagerController.java
@@ -1,16 +1,25 @@
 package Controller;
 
+import Model.UserRepository;
+import Model.employee;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.SplitPane;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class FlightManagerController implements Initializable {
+
+    UserRepository userRepository = new UserRepository();
+
+    private String ID;
+    Button newAirplane;
 
     @FXML
     Button flightsBTN;
@@ -20,7 +29,8 @@ public class FlightManagerController implements Initializable {
     Button airplaneBTN;
     @FXML
     Button NewFlightBTN;
-
+    @FXML
+    VBox vbox;
 
     public void flightsBTN() {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/FlightsListBTN.fxml"));
@@ -32,14 +42,6 @@ public class FlightManagerController implements Initializable {
         split.getItems().set(1, loader.getRoot());
     }
 
-    public void flightsBTNEnter() {
-        flightsBTN.setStyle("-fx-background-color: #3679c6");
-    }
-
-    public void flightsBTNExit() {
-        flightsBTN.setStyle("-fx-background-color: transparent ");
-    }
-
     public void airplaneBTN() {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/airplaneList.fxml"));
 
@@ -48,39 +50,78 @@ public class FlightManagerController implements Initializable {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        airplaneListController airplaneListController = loader.getController();
+        airplaneListController.setJob(getJob());
+        airplaneListController.loginAs();
+
         split.getItems().set(1, loader.getRoot());
+
     }
 
-    public void airplaneBTNEnter() {
-        airplaneBTN.setStyle("-fx-background-color: #3679c6");
-    }
-
-    public void airplaneBTNExit() {
-        airplaneBTN.setStyle("-fx-background-color: transparent");
-    }
-
-    public void NewFlightBTN(){
+    public void NewFlightBTN() {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/NewFlight.fxml"));
         try {
             loader.load();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        split.getItems().set(1 , loader.getRoot());
-    }
-
-    public void NewFlightBTNEnter(){
-        NewFlightBTN.setStyle("-fx-background-color: #3679c6");
-    }
-
-    public void NewFlightBTNExit(){
-        NewFlightBTN.setStyle("-fx-background-color: transparent ");
+        split.getItems().set(1, loader.getRoot());
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-//        split.setDividerPositions(0.2);
         split.lookupAll(".split-pane-divider").stream()
                 .forEach(div -> div.setMouseTransparent(true));
+    }
+
+    public void management() {
+        if (getJob().equals("Management")) {
+            newAirplane = new Button("new airplane");
+            newAirplane.getStylesheets().add("/resource/flightMenuBTN.css");
+            vbox.getChildren().add(newAirplane);
+            newAirplaneBTNAction(newAirplane);
+        }
+
+    }
+
+    private void newAirplaneBTNAction(Button btn) {
+
+        btn.setOnMouseClicked(event -> {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/NewAirplane.fxml"));
+            try {
+                loader.load();
+            } catch (IOException e) {
+                System.out.println("" + e);
+            }
+            NewAirplaneController newAirplaneController = loader.getController();
+            newAirplaneController.setID(getID());
+
+            split.getItems().set(1, loader.getRoot());
+        });
+
+    }
+
+    public String getJob() {
+        List<employee> employees = userRepository.employer();
+        String job = null;
+        for (int i = 0; i < employees.size(); i++) {
+
+            if (employees.get(i).getID().equals(ID)) {
+                job = String.valueOf(employees.get(i).getJob());
+                break;
+            }
+
+        }
+
+        return job;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
     }
 }
