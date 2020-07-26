b0e1eb5ead386000fefd08aa5b8c14bb596539a7
Mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2020-07-27 00:19:19 +0430

    1. this page will be show that persion login and can edit or complite the profile 2.can't change the salary just see 3.

---
+++ src/Controller/EmployeeProfileController.java
@@ -0,0 +1,183 @@
+package Controller;
+
+import Model.Support;
+import Model.SupportRepository;
+import Model.UserRepository;
+import Model.employee;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class EmployeeProfileController implements Initializable {
+    UserRepository userRepository = new UserRepository();
+
+    private String ID;
+    private boolean editClicked = false;
+
+    @FXML
+    TextField nameField;
+    @FXML
+    TextField idField;
+    @FXML
+    TextField phoneNumberField;
+    @FXML
+    TextField usernameField;
+    @FXML
+    PasswordField passwordField;
+    @FXML
+    PasswordField rePassField;
+    @FXML
+    TextField addressField;
+    @FXML
+    TextField emailField;
+    @FXML
+    TextField salaryField;
+    @FXML
+    Button submitBTN;
+    @FXML
+    Circle circel;
+    @FXML
+    Button editBTN;
+    @FXML
+    VBox rePass;
+
+    public void submitBTN() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "do you want to submit??", ButtonType.YES, ButtonType.NO);
+        alert.showAndWait();
+        if (alert.getResult() == ButtonType.YES) {
+            boolean update = userRepository.infoUpdate(salaryField.getText(), addressField.getText(), emailField.getText(), phoneNumberField.getText(), usernameField.getText(), passwordField.getText());
+            if (update) {
+                Alert alert1 = new Alert(Alert.AlertType.INFORMATION, "done", ButtonType.CLOSE);
+                alert1.showAndWait();
+                ((Stage) submitBTN.getScene().getWindow()).close();
+            } else {
+                Alert alert1 = new Alert(Alert.AlertType.INFORMATION, "cant update...", ButtonType.CLOSE);
+                alert1.showAndWait();
+            }
+        } else {
+            Alert alert1 = new Alert(Alert.AlertType.CONFIRMATION, "canceled", ButtonType.CLOSE);
+            alert1.showAndWait();
+        }
+
+    }
+
+    public void editBTN() {
+        editClicked = true;
+        phoneNumberField.setEditable(true);
+        if (!phoneNumberField.getText().isEmpty()) {
+            phoneNumberField.setStyle("-fx-background-color: transparent ; -fx-border-width: 0 0 0 3 ; -fx-border-color: green");
+        } else {
+            phoneNumberField.setStyle("-fx-background-color: transparent ; -fx-border-width: 0 0 0 3 ; -fx-border-color: yellow");
+
+        }
+
+        usernameField.setEditable(true);
+        usernameField.setStyle("-fx-background-color: transparent ; -fx-border-width: 0 0 0 3 ; -fx-border-color: green");
+
+        passwordField.setEditable(true);
+        passwordField.setStyle("-fx-background-color: transparent ; -fx-border-width: 0 0 0 3 ; -fx-border-color: green");
+
+
+        addressField.setEditable(true);
+        if (!addressField.getText().isEmpty()) {
+            addressField.setStyle("-fx-background-color: transparent ; -fx-border-width: 0 0 0 3 ; -fx-border-color: green");
+        } else {
+            addressField.setStyle("-fx-background-color: transparent ; -fx-border-width: 0 0 0 3 ; -fx-border-color: yellow");
+
+        }
+
+        emailField.setEditable(true);
+        if (!emailField.getText().isEmpty()) {
+            emailField.setStyle("-fx-background-color: transparent ; -fx-border-width: 0 0 0 3 ; -fx-border-color: green");
+        } else {
+            emailField.setStyle("-fx-background-color: transparent ; -fx-border-width: 0 0 0 3 ; -fx-border-color: yellow");
+
+        }
+
+        submitBTN.setDisable(false);
+    }
+
+    public void passwordField() {
+        if (editClicked) {
+            rePass.setDisable(false);
+            rePassField.setStyle("-fx-background-color: transparent ; -fx-border-width: 0 0 0 3 ; -fx-border-color: green");
+
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        circel.setFill(new ImagePattern(new Image("file:Icons/aaa.png")));
+        editBTN.setGraphic(new ImageView("file:Icons/edit.png"));
+    }
+
+    public void fillField() {
+        List<employee> employees = userRepository.employer();
+        for (int i = 0; i < employees.size(); i++) {
+            if (employees.get(i).getID().equals(getID())) {
+                nameField.setText(employees.get(i).getFirstName() + " " + employees.get(i).getLastName());
+                idField.setText(getID());
+                phoneNumberField.setText(employees.get(i).getPhoneNumber());
+                usernameField.setText(employees.get(i).getUsername());
+                passwordField.setText(employees.get(i).getPassword());
+                addressField.setText(employees.get(i).getAddress());
+                emailField.setText(employees.get(i).getEmail());
+                salaryField.setText(setSalary(employees.get(i).getSalary()));
+                break;
+            }
+        }
+    }
+
+    private String setSalary(String salary) {
+        char[] a = salary.toCharArray();
+        int counter = 0;
+
+        int b = a.length;
+        int c = b / 3 + b;
+        String[] d = new String[c];
+        int j = c;
+        for (int i = (a.length - 1); i <= a.length; i--) {
+            j--;
+            if (counter == 3) {
+                d[j] = ",";
+                i++;
+                counter = -1;
+            } else {
+                d[j] = String.valueOf(a[i]);
+
+            }
+            if (i == 0)
+                break;
+            counter++;
+
+        }
+
+        String Final = "";
+
+        for (int i = 0; i < c; i++) {
+            Final += d[i];
+        }
+
+        return Final;
+
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+}
