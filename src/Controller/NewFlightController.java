d5f7e28c6178199ea7b18f31085eaec125fb8cca
Mohammad Kz <mohammad.mehdi.khz@gmail.com>
2020-08-05 08:35:43 +0430

    1. function setNull added ==> after add the new info to DB all field will be null 2. after make the all field null => should make the border as normall 3. gralhics changed 4. Alert and .... added to add btn ==> if can update so will send the info to user if not show the error message

--- src/Controller/NewFlightController.java
+++ src/Controller/NewFlightController.java
@@ -6,10 +6,7 @@ import Model.Flight;
 import Model.FlightRepository;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.paint.Color;
 
 import java.net.URL;
@@ -44,9 +41,19 @@ public class NewFlightController implements Initializable {
     @FXML
     Button addBTN;
 
+    // add the new flight
     public void addBTN() {
         boolean check = flightRepository.newFlight(FNField.getText().toUpperCase(), registerField.getText().toUpperCase(), depField.getText().toUpperCase(), desField.getText().toUpperCase(), datePicker.getValue().toString(),
                 hoursField.getText(), flightTimeField.getText(), "open", capacity, capacity, Integer.parseInt(priceField.getText()));
+        if (check) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "done", ButtonType.CLOSE);
+            alert.showAndWait();
+            setNull();
+            setBorder();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "can't add ", ButtonType.OK);
+            alert.showAndWait();
+        }
     }
 
     @Override
@@ -54,8 +61,9 @@ public class NewFlightController implements Initializable {
         TextFieldAction();
     }
 
+    // set the text field action ==> register , FN , dep , des
     private void TextFieldAction() {
-
+        // just can input 6 char
         registerField.setOnKeyReleased(event -> {
 
             if (registerField.getLength() == 6) {
@@ -65,7 +73,7 @@ public class NewFlightController implements Initializable {
                         capacityLBL.setText("capacity: " + airplanes.get(i).getSeats());
                         capacity = airplanes.get(i).getSeats();
                         capacityLBL.setTextFill(Color.BLUE);
-                        registerField.setStyle("-fx-border-width: 0 0 3 0 ; -fx-border-color: green");
+                        registerField.setStyle("-fx-border-width: 2 2 2 2 ; -fx-border-color: green ; -fx-border-radius: 30 ; -fx-background-radius: 30");
                         break;
                     } else if (i == (airplanes.size() - 1)) {
                         capacityLBL.setText("can't find this aircraft..");
@@ -75,36 +83,56 @@ public class NewFlightController implements Initializable {
 
             }
         });
-
+        // when start typing set the register field border
         registerField.setOnKeyPressed(event -> {
-            registerField.setStyle("-fx-border-width: 0 0 3 0 ; -fx-border-color: yellow");
+            registerField.setStyle("-fx-border-width: 2 2 2 2 ; -fx-border-color: yellow; -fx-border-radius: 30 ; -fx-background-radius: 30");
         });
-
+        // check the flight number ==> is there or not
         FNField.setOnKeyReleased(event -> {
 
             List<Flight> flights = flightRepository.flightList();
 
             for (int i = 0; i < flights.size(); i++) {
                 if (flights.get(i).getFlightNumber().equals(FNField.getText().toUpperCase())) {
-                    FNField.setStyle("-fx-border-width: 0 0 3 0 ; -fx-border-color: red");
+                    FNField.setStyle("-fx-border-width: 2 2 2 2 ; -fx-border-color: red; -fx-border-radius: 30 ; -fx-background-radius: 30");
                     break;
                 } else {
-                    FNField.setStyle("-fx-border-width: 0 0 3 0 ; -fx-border-color: green");
+                    FNField.setStyle("-fx-border-width: 2 2 2 2 ; -fx-border-color: green; -fx-border-radius: 30 ; -fx-background-radius: 30");
                 }
             }
 
 
         });
-
+        //when start typing set the dep field border
         depField.setOnKeyReleased(event -> {
-            depField.setStyle("-fx-border-width: 0 0 3 0 ; -fx-border-color: green");
+            depField.setStyle("-fx-border-width: 2 2 2 2 ; -fx-border-color: green; -fx-border-radius: 30 ; -fx-background-radius: 30");
         });
-
+        //when start typing set the des field border
         desField.setOnKeyReleased(event -> {
-            desField.setStyle("-fx-border-width: 0 0 3 0 ; -fx-border-color: green");
+            desField.setStyle("-fx-border-width: 2 2 2 2 ; -fx-border-color: green; -fx-border-radius: 30 ; -fx-background-radius: 30");
         });
 
+    }
 
+    //after add the new flight to DB should make the field null(empty)
+    private void setNull() {
+        FNField.setText(null);
+        registerField.setText(null);
+        depField.setText(null);
+        desField.setText(null);
+        hoursField.setText(null);
+        flightTimeField.setText(null);
+        priceField.setText(null);
+        capacityLBL.setText(null);
+    }
+    // set the border 0 0 0 0 after click on the add
+    private void setBorder(){
+        FNField.setStyle("-fx-border-radius: 30 ; -fx-border-width: 0 0 0 0;  -fx-border-radius: 30 ; -fx-background-radius: 30");
+        registerField.setStyle("-fx-border-radius: 30 ; -fx-border-width: 0 0 0 0; -fx-border-radius: 30 ; -fx-background-radius: 30 ");
+        depField.setStyle("-fx-border-radius: 30 ; -fx-border-width: 0 0 0 0;  -fx-border-radius: 30 ; -fx-background-radius: 30");
+        hoursField.setStyle("-fx-border-radius: 30 ; -fx-border-width: 0 0 0 0; -fx-border-radius: 30 ; -fx-background-radius: 30 ");
+        flightTimeField.setStyle("-fx-border-radius: 30 ; -fx-border-width: 0 0 0 0;  -fx-border-radius: 30 ; -fx-background-radius: 30");
+        priceField.setStyle("-fx-border-radius: 30 ; -fx-border-width: 0 0 0 0; -fx-border-radius: 30 ; -fx-background-radius: 30 ");
     }
 
 }
