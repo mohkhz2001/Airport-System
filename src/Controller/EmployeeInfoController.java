0fd76dc1908b8c7636607c9cd391c9f9abd876b5
Mohammad Kz <mohammad.mehdi.khz@gmail.com>
2020-08-05 19:50:05 +0430

    1. super admin can changed the job for the user(BTN add) 2. job field typed removed 3. new function that return the job added

--- src/Controller/EmployeeInfoController.java
+++ src/Controller/EmployeeInfoController.java
@@ -21,6 +21,7 @@ public class EmployeeInfoController implements Initializable {
     UserRepository userRepository = new UserRepository();
 
     private String ID;
+    private String userID;
     private boolean check;
     private Long salary;
 
@@ -49,6 +50,8 @@ public class EmployeeInfoController implements Initializable {
     Button decreaseSalaryBTN;
     @FXML
     Button increaseSalaryBTN;
+    @FXML
+    Button changJob;
 
     /*
         if anythings changed of the info the exit btn will be the changed to updateBTN
@@ -90,7 +93,7 @@ public class EmployeeInfoController implements Initializable {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "do you really want to fired this employee??!", ButtonType.YES, ButtonType.NO);
         alert.showAndWait();
         if (alert.getResult() == ButtonType.YES) {
-            boolean remove = userRepository.removeEmployee(getID());
+            boolean remove = userRepository.removeEmployee(getUserID());
             Alert alert1;
             if (remove) {
                 alert1 = new Alert(Alert.AlertType.INFORMATION, "Done", ButtonType.OK);
@@ -106,8 +109,15 @@ public class EmployeeInfoController implements Initializable {
         }
     }
 
-    public void jobFieldType() {
-
+    // just super admin can change the usr job.....
+    public void changJob() {
+        if (jobChangeCheck().equals(Person.Job.superAdmin)){
+            if (jobField.getText().equals(String.valueOf(Person.Job.Employee))){
+                jobField.setText(String.valueOf(Person.Job.Management));
+            }else if (jobField.getText().equals(String.valueOf(Person.Job.Management))){
+                jobField.setText(String.valueOf(Person.Job.Employee));
+            }
+        }
     }
 
     // decease the salary
@@ -144,6 +154,7 @@ public class EmployeeInfoController implements Initializable {
         emailField.setText(employee.getEmail());
         phoneNumberField.setText(employee.getPhoneNumber());
 
+        setUserID(employee.getID());
     }
 
     // if the changed the info EXP(salary) ==> the exit BTN will be changed to update BTN
@@ -186,6 +197,18 @@ public class EmployeeInfoController implements Initializable {
         return Final;
 
     }
+    // return the job to  check the job
+    private Person.Job jobChangeCheck() {
+        List<employee> employees = userRepository.employer();
+        Person.Job job = null;
+        for (int i = 0; i < employees.size(); i++) {
+            if (employees.get(i).getID().equals(getID())) {
+                job = employees.get(i).getJob();
+                break;
+            }
+        }
+        return job;
+    }
 
     public String getID() {
         return ID;
@@ -210,5 +233,13 @@ public class EmployeeInfoController implements Initializable {
 
     public void setSalary(Long salary) {
         this.salary = salary;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
     }
 }
