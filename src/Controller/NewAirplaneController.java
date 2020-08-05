b1ff7ebf430fbdf8fd23e38c82bd196bb5a2b7d9
Mohammad Kz <mohammad.mehdi.khz@gmail.com>
2020-08-05 21:04:19 +0430

    1 comment added 2.

--- src/Controller/NewAirplaneController.java
+++ src/Controller/NewAirplaneController.java
@@ -41,37 +41,43 @@ public class NewAirplaneController implements Initializable {
     @FXML
     Button addBTN;
 
-
+    // start typing in register field
     public void registerFieldType() {
         registerField.setStyle("-fx-border-width: 0 0 3 0 ; -fx-border-color: green ; -fx-background-color:  transparent");
     }
 
+    // max get the 6 char
     public void registerFieldPresse() {
         addTextLimiter(registerField, 6);
     }
 
+    // max get the 8 char
     public void typeFieldPressed() {
         addTextLimiter(typeField, 8);
     }
 
+    // start typing in type of aircraft
     public void typeFieldType() {
         typeField.setStyle("-fx-border-width: 0 0 3 0 ; -fx-border-color: green ; -fx-background-color:  transparent");
     }
 
+    // max get the 4 char
     public void seatsFieldPressed() {
         addTextLimiter(seatsField, 4);
     }
 
+    // start typing in seats field
     public void seatsFieldType() {
         seatsField.setStyle("-fx-border-width: 0 0 3 0 ; -fx-border-color: green ; -fx-background-color:  transparent");
     }
 
+    // click on the add btn
     public void addBTN() {
 
         AirplaneRepository airplaneRepository = new AirplaneRepository();
         List<Airplane> airplanes = airplaneRepository.airplaneList();
 
-        boolean newRegister = false;
+        boolean newRegister = false;// check is it new or not
         for (int i = 0; i < airplanes.size(); i++) {
 
             if (airplanes.get(i).getRegister().equals(registerField.getText().toUpperCase())) {
@@ -81,7 +87,7 @@ public class NewAirplaneController implements Initializable {
             } else if (i == airplanes.size() - 1)
                 newRegister = true;
         }
-
+        //if non of the field are empty => will add this to DB
         if (newRegister && !registerField.getText().isEmpty() && !typeField.getText().isEmpty() && !seatsField.getText().isEmpty()) {
             boolean check = airplaneRepository.addNewAirplane(registerField.getText().toUpperCase(), typeField.getText().toUpperCase(), seatsField.getText());
 
@@ -94,7 +100,9 @@ public class NewAirplaneController implements Initializable {
                 LBL.setStyle("-fx-background-color: red");
             }
 
-        } else {
+        }
+        // if on of the field was empty => make that border red
+        else {
             if (registerField.getText().isEmpty()) {
                 registerField.setStyle("-fx-background-color: transparent ; -fx-border-color: red ; -fx-border-width:  0 0 3 0");
                 registerLBL.setText("can't be empty");
@@ -118,9 +126,11 @@ public class NewAirplaneController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        // set the image...
         image.setImage(new Image("file:Icons/newAirplane.jpeg"));
     }
 
+    // et the field length
     public static void addTextLimiter(final TextField tf, final int maxLength) {
         tf.textProperty().addListener(new ChangeListener<String>() {
             @Override
