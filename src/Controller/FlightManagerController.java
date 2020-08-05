ac6618e130a32268fec57f540eaa6e0d12c0bf75
Mohammad Kz <mohammad.mehdi.khz@gmail.com>
2020-08-05 08:28:24 +0430

    1. comment added 2. getjob return => job not String 3. conditions of the management are changed

--- src/Controller/FlightManagerController.java
+++ src/Controller/FlightManagerController.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import Model.Person;
 import Model.UserRepository;
 import Model.employee;
 import javafx.fxml.FXML;
@@ -7,6 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.SplitPane;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
@@ -31,7 +33,7 @@ public class FlightManagerController implements Initializable {
     Button NewFlightBTN;
     @FXML
     VBox vbox;
-
+        // flightBTN action handler ==> show the all flight
     public void flightsBTN() {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/FlightsListBTN.fxml"));
         try {
@@ -41,7 +43,7 @@ public class FlightManagerController implements Initializable {
         }
         split.getItems().set(1, loader.getRoot());
     }
-
+        // airplaneBTN action handler ==> show the airplane and if click each airplane show the flights
     public void airplaneBTN() {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/airplaneList.fxml"));
 
@@ -58,7 +60,7 @@ public class FlightManagerController implements Initializable {
         split.getItems().set(1, loader.getRoot());
 
     }
-
+        // newFlightBTN ==> make the new flight.
     public void NewFlightBTN() {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/NewFlight.fxml"));
         try {
@@ -71,20 +73,24 @@ public class FlightManagerController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        // set the icon for the btn
+        flightsBTN.setGraphic(new ImageView("file:Icons/flights2.png"));
+        airplaneBTN.setGraphic(new ImageView("file:Icons/airplane2.png"));
+//        newAirplane.setGraphic(new ImageView("file:Icons/"));
         split.lookupAll(".split-pane-divider").stream()
                 .forEach(div -> div.setMouseTransparent(true));
     }
-
+        //if the login was managment or super admin add the new airplane btn => can add new airplane for flights
     public void management() {
-        if (getJob().equals("Management") || getJob().equals("superAdmin")) {
-            newAirplane = new Button("new airplane");
+        if (getJob().equals(Person.Job.Management) || getJob().equals(Person.Job.superAdmin)) {
+            newAirplane = new Button("new Airplane");
             newAirplane.getStylesheets().add("/resource/flightMenuBTN.css");
             vbox.getChildren().add(newAirplane);
             newAirplaneBTNAction(newAirplane);
         }
 
     }
-
+        // action on the airplane BTN
     private void newAirplaneBTNAction(Button btn) {
 
         btn.setOnMouseClicked(event -> {
@@ -101,14 +107,14 @@ public class FlightManagerController implements Initializable {
         });
 
     }
-
-    public String getJob() {
+        // get the job ==> return the job as String
+    public Person.Job getJob() {
         List<employee> employees = userRepository.employer();
-        String job = null;
+        Person.Job job = null;
         for (int i = 0; i < employees.size(); i++) {
 
             if (employees.get(i).getID().equals(ID)) {
-                job = String.valueOf(employees.get(i).getJob());
+                job = employees.get(i).getJob();
                 break;
             }
 
@@ -116,11 +122,11 @@ public class FlightManagerController implements Initializable {
 
         return job;
     }
-
+        //get the ID
     public String getID() {
         return ID;
     }
-
+        //set the ID
     public void setID(String ID) {
         this.ID = ID;
     }
